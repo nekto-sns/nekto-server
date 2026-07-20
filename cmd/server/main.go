cb794ddf3ca58c5b291486f9331f2b42f568f5c1
Itsuki0222 <175463719+Itsuki0222@users.noreply.github.com>
2026-07-20 10:30:22 +0900

    feat: Hello Worldを返すサーバーを追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/nekto-sns/nekto-server/app/handlers"
+)
+
 func main() {
+	r := chi.NewRouter()
+	r.Get("/hello", handlers.Hello)
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", r)
 }
