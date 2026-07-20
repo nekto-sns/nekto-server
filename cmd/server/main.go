06bc1209c4c0a8fa555757b466f31741d6181875
Itsuki0222 <175463719+Itsuki0222@users.noreply.github.com>
2026-07-20 13:41:45 +0900

    refactor: handlersディレクトリをhandlerに変更

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
-	"github.com/nekto-sns/nekto-server/app/handlers"
+	"github.com/nekto-sns/nekto-server/app/handler"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/hello", handlers.Hello)
+	r.Get("/hello", handler.Hello)
 	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", r)
 }
